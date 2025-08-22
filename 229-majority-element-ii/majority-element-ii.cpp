07e2c20e06c5f56b6befb9bbdd55074482e7dac5
Aastha_am <aastha.mewar2801@gmail.com>
2025-08-22 23:39:49 +0530

    Time: 15 ms (11.34%) | Memory: 27 MB (10.27%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        // vector<int> ls;
+        // map<int,int> mpp;
+        // int n=nums.size();
+        // int mini=(int)(n/3)+1;
+        // for(int i=0;i<n;i++){
+        //     mpp[nums[i]]++;
+        //     if(mpp[nums[i]]==mini){
+        //         ls.push_back(nums[i]);
+        //     }
+        //     if(ls.size()==2)break;
+        // }
+        // sort(ls.begin(),ls.end());
+        // return ls;
+        vector<int> ls;
+        map<int,int> mp;
+        int n = nums.size();
+        int mini = (int)(n/3)+1;
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+            if(mp[nums[i]] == mini){
+                ls.push_back(nums[i]);
+            }
+            if(ls.size()==2)break;
+        }
+        sort(ls.begin(),ls.end());
+        return ls;
+    }
+};
