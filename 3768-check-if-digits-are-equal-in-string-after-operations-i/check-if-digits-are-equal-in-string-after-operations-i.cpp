c3eed1116677bceae3df22bcbbd04422797dc57c
Aastha_am <aastha.mewar2801@gmail.com>
2025-10-23 23:39:56 +0530

    Time: 35 ms (11.11%) | Memory: 11.3 MB (18.52%) - LeetSync

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        
+        while(s.size()>2){
+           string ans="";
+           for(int i=0;i<s.size()-1;i++){
+            int sum = (s[i]-'0'+s[i+1]-'0')%10;
+            ans += to_string(sum);
+           }
+           s = ans;
+        }
+        return s[0]==s[1];
+    }
+};
