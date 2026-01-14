287ba40c1b38711de27d56d50e406e07c88b0db6
Aastha_am <aastha.mewar2801@gmail.com>
2026-01-14 11:28:37 +0530

    Time: 1 ms (35.11%) | Memory: 22.9 MB (70.20%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,30 +2,27 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        
-        // Edge case
-        if (n == 1) return 1;
-
-        vector<int> candies(n, 1);
-
-        // Left -> Right
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
+        vector<int>candy(n,1); //initially 1
+        int sum=0;
+        // checking left to right to find the higher rating
+        for(int i=1;i<n;i++)
+        {
+            if(ratings[i]>ratings[i-1]){
+                candy[i]=candy[i-1]+1;
             }
         }
 
-        // Right -> Left
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                candies[i] = max(candies[i], candies[i + 1] + 1);
+        // checking right to left to find the higher rating
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                candy[i]=max(candy[i],candy[i+1]+1);
             }
         }
 
-        int total = 0;
-        for (int c : candies) total += c;
-
-        return total;
-
+        // summation
+        for(int i=0;i<n;i++){
+            sum+=candy[i];
+        }
+        return sum;
     }
 };
