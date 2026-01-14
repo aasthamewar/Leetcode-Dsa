9a9091b191e587937747fd49abce1d80b5ef046c
Aastha_am <aastha.mewar2801@gmail.com>
2026-01-14 21:23:29 +0530

    Time: 7 ms (28.98%) | Memory: 12.8 MB (19.24%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<pair<int, string>> roman = {
-            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-            {100, "C"},  {90, "XC"},  {50, "L"},  {40, "XL"},
-            {10, "X"},   {9, "IX"},   {5, "V"},   {4, "IV"},
-            {1, "I"}
+        // vector<pair<int, string>> roman = {
+        //     {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+        //     {100, "C"},  {90, "XC"},  {50, "L"},  {40, "XL"},
+        //     {10, "X"},   {9, "IX"},   {5, "V"},   {4, "IV"},
+        //     {1, "I"}
+        // };
+        // string ans="";
+        // for(int i=0;i<roman.size();i++){
+        //     int value=roman[i].first;
+        //     string symbol=roman[i].second;
+
+        //     while(num>=value){
+        //         ans+=symbol;
+        //         num-=value;
+        //     }
+        // }
+        // return ans;
+
+        vector<pair<int, string>> roman={
+            {1000,"M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
         };
-        string ans="";
+        string ans = "";
         for(int i=0;i<roman.size();i++){
-            int value=roman[i].first;
-            string symbol=roman[i].second;
+            int value = roman[i].first;
+            string symbol = roman[i].second;
 
             while(num>=value){
                 ans+=symbol;
