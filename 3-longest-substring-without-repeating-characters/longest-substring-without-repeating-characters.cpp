13e8eca6af21cae42d598d8b20ee25d9982c1c7e
Aastha_am <aastha.mewar2801@gmail.com>
2026-01-20 23:24:11 +0530

    Time: 688 ms (5.00%) | Memory: 247.4 MB (5.13%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,40 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // unordered_set<char> seen;
-        // int left = 0, right = 0, maxlen = 0;
-
-        // while (right < s.size()) {
-        //     // if character not seen we will expand the window
-        //     if (seen.find(s[right]) == seen.end()) {
-        //         seen.insert(s[right]);
-        //         maxlen = max(maxlen, right - left + 1);
-        //         right++;
-        //     }
-        //     // if duplicate found , we will shrink window from left
-        //     else {
-        //         seen.erase(s[left]);
-        //         left++;
-        //     }
-        // }
-
-        // return maxlen;
-        unordered_set<char> seen;
-        int right=0,left=0,maxlen=0;
-
-        while(right<s.size()){
-            //first we will check if the char r not duplicate
-            if(seen.find(s[right])==seen.end()){
-                seen.insert(s[right]);
-                maxlen=max(maxlen,right-left+1);
-                right++;
-            }
-            //if the char is duplicate for that
-            else{
-                seen.erase(s[left]);
-                left++;
+        //brute force approach
+        int maxi=0;
+        
+        for(int i=0;i<s.size();i++){
+            unordered_set<char> st;
+            for(int j=i;j<s.size();j++){
+                if(st.count(s[j])) // duplicate finding
+                {
+                    break;
+                }
+                st.insert(s[j]);
+                maxi=max(maxi,j-i+1);
             }
         }
-        return maxlen;
+        return maxi;
     }
 };
