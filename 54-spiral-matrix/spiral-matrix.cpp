042ba14901382a4426efc2cf5d9a0e48086f4689
Aastha_am <aastha.mewar2801@gmail.com>
2026-01-21 21:14:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (14.30%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -6,13 +6,51 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
 
-        // Define boundaries
+        // // Define boundaries
+        // int top = 0;
+        // int bottom = m - 1;
+        // int left = 0;
+        // int right = n - 1;
+
+        // // Traverse while there is a valid rectangle
+        // while (top <= bottom && left <= right) {
+
+        //     // 1️⃣ Move LEFT → RIGHT (top row)
+        //     for (int col = left; col <= right; col++) {
+        //         result.push_back(matrix[top][col]);
+        //     }
+        //     top++;   // top row is done
+
+        //     // 2️⃣ Move TOP → BOTTOM (right column)
+        //     for (int row = top; row <= bottom; row++) {
+        //         result.push_back(matrix[row][right]);
+        //     }
+        //     right--; // right column is done
+
+        //     // 3️⃣ Move RIGHT → LEFT (bottom row)
+        //     if (top <= bottom) {
+        //         for (int col = right; col >= left; col--) {
+        //             result.push_back(matrix[bottom][col]);
+        //         }
+        //         bottom--; // bottom row is done
+        //     }
+
+        //     // 4️⃣ Move BOTTOM → TOP (left column)
+        //     if (left <= right) {
+        //         for (int row = bottom; row >= top; row--) {
+        //             result.push_back(matrix[row][left]);
+        //         }
+        //         left++; // left column is done
+        //     }
+        // }
+
+        // return result;
+
         int top = 0;
         int bottom = m - 1;
         int left = 0;
         int right = n - 1;
 
-        // Traverse while there is a valid rectangle
         while (top <= bottom && left <= right) {
 
             // 1️⃣ Move LEFT → RIGHT (top row)
@@ -43,7 +81,6 @@ public:
                 left++; // left column is done
             }
         }
-
         return result;
     }
 };
