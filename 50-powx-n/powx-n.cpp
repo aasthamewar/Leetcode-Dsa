836105ecfe9dd47b264e7ade5137e41b65d9ba03
Aastha_am <aastha.mewar2801@gmail.com>
2026-03-28 00:16:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.28%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    double myPow(double x, long long n) {
-        if (n == 0) return 1.0;
+    double myPow(double x, long long int n) {
+        if(n==0)return 1.00;
 
-        // Handle negative exponent
-        if (n < 0) {
-            x = 1 / x;
+        //if negative n
+        if(n<0){
+            x = 1/x;
             n = -n;
         }
-
-        double result = 1.0;
-        while (n > 0) {
-            if (n % 2 == 1) {     // If n is odd
-                result *= x;
+        double ans = 1.00;
+        while(n>0){
+            if(n%2==1){
+                ans = ans*x;
             }
-            x *= x;               // Square the base
-            n /= 2;               // Divide exponent by 2
+            x = x*x;
+            n /= 2;
         }
-        return result;
+        return ans;
     }
 };
