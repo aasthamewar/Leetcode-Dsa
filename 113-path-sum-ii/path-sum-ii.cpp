4b145a60ed0c5c152f673d30c7591beb9382ce7d
Aastha_am <aastha.mewar2801@gmail.com>
2026-06-13 11:28:41 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (39.07%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -6,36 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void solve(TreeNode* root, int targetSum, vector<int>& path,
-               vector<vector<int>>& ans) {
-        
-        if(root==NULL)return ;
-
-        path.push_back(root->val);
-
-        if (root->left == NULL && root->right == NULL) {
-            if (targetSum == root->val) {
-                ans.push_back(path);
-            }
-            
+void solve(TreeNode* root, int targetSum, vector<int>& path, vector<vector<int>>& ans){
+    if(root==NULL)return ;
+    path.push_back(root->val);
+    if(root->right == NULL && root->left == NULL){
+        if(targetSum==root->val){
+            ans.push_back(path);
         }
-        else{
-            solve(root->left, targetSum - root->val, path, ans);
-            solve(root->right, targetSum - root->val, path, ans);
-        }
-
-        path.pop_back();
     }
+    else{
+        solve(root->left,targetSum-root->val,path,ans);
+        solve(root->right,targetSum-root->val,path,ans);
+    }
+    path.pop_back();
+
+}
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<int> path;
-        vector<vector<int>> ans;
-        solve(root, targetSum, path, ans);
+        vector<vector<int>>ans;
+        vector<int>path;
+        solve(root,targetSum,path,ans);
         return ans;
     }
 };
