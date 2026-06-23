0c2f02123f5137fadc13fc19ffe8e8405a8294b2
Aastha_am <aastha.mewar2801@gmail.com>
2026-06-23 23:39:18 +0530

    Time: 0 ms (100.00%) | Memory: 63 MB (10.63%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -4,19 +4,17 @@ public:
         int maxiwater=0;
         int start=0,end=height.size()-1;
         while(start<end){
-            int width=end-start;
-            int effective_height=min(height[start],height[end]);
-            int area=width*effective_height;
+            int width = end-start;
+            int effective_height = min(height[start],height[end]);
+            int area = width * effective_height;
 
-            // update the area
-            maxiwater=max(maxiwater,area);
-
-            // move the pointer
+            // updating the maxwater
+            maxiwater = max(maxiwater,area);
             if(height[start]<height[end]){
-                start++;
+            start++;
             }
             else{
-                end--;
+            end--;
             }
         }
         return maxiwater;
