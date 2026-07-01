58496bf3053553b36866fd19915136020978db2b
Aastha_am <aastha.mewar2801@gmail.com>
2026-07-01 23:49:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (81.34%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+
+    bool dfs(int src,
+             vector<vector<int>>& adj,
+             vector<bool>& vis,
+             vector<bool>& rec)
+    {
+        vis[src] = true;
+        rec[src] = true;
+
+        for(int v : adj[src])
+        {
+            if(!vis[v])
+            {
+                if(dfs(v, adj, vis, rec))
+                    return true;
+            }
+            else if(rec[v])
+            {
+                return true;
+            }
+        }
+
+        rec[src] = false;
+        return false;
+    }
+
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+
+        vector<vector<int>> adj(n);
+
+        for(auto &e : prerequisites)
+            adj[e[1]].push_back(e[0]);
+
+        vector<bool> vis(n,false);
+        vector<bool> rec(n,false);
+
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                if(dfs(i, adj, vis, rec))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
